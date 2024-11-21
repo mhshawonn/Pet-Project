3193a81acefa13391b35672d4dce083e6372f6b0
AR-Hridoy-7 <arhridoy2002@gmail.com>
2024-11-22 01:21:41 +0600

    Chat added

--- Backend on/Backend/src/main/java/com/pet/Pet/Repo/TagRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pet.Pet.Repo;
-
-import com.pet.Pet.Model.Tags;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepo extends JpaRepository<Tags, Integer> {
-}
